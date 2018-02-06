da5e3fbad006bec73914e859e2efdf3b8965810f
Christian Schiffler <c.schiffler@cyberspectrum.de>
2018-02-06 03:55:14 +0100

    Move language strings of system cols to core
    See:
    - MetaModels/attribute_alias@03a65cc
    - MetaModels/attribute_alias#22
    - MetaModels/core@ba24a63

--- contao/dca/tl_metamodel_attribute.php
+++ contao/dca/tl_metamodel_attribute.php
@@ -97,7 +97,7 @@ $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['talias_fields'] = array
                 'label'                 => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute'],
                 'exclude'               => true,
                 'inputType'             => 'select',
-                'reference'             => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values'],
+                'reference'             => &$GLOBALS['TL_LANG']['METAMODELS_SYSTEM_COLUMNS'],
                 'eval' => array
                 (
                     'style'             => 'width:600px',
