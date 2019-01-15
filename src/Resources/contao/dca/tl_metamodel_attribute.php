bfe52f78409460729e1a4c5dd9a228045779da15
Christian Schiffler <c.schiffler@cyberspectrum.de>
2019-01-15 16:41:11 +0100

    Fix code style

--- src/Resources/contao/dca/tl_metamodel_attribute.php
+++ src/Resources/contao/dca/tl_metamodel_attribute.php
@@ -11,13 +11,13 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    MetaModels/attribute_translatedalias
- * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @author      Stefan Heimes <stefan_heimes@hotmail.com>
- * @author      Andreas Isaak <info@andreas-isaak.de>
- * @author      Sven Baumann <baumann.sv@gmail.com>
- * @author      David Molineus <david.molineus@netzmacht.de>
- * @author      Ingolf Steinhardt <info@e-spin.de>
- * @author      Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Andreas Isaak <info@andreas-isaak.de>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @copyright  2012-2019 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedalias/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
