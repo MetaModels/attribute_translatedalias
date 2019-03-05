03d8f5d8318bddbb7d1b191b449d7f0c3fada577
Mini Model <minimodel@metamodel.me>
2019-03-05 19:40:57 +0100

    Update PHPDoc file headers

--- tests/DeprecatedAutoloaderTest.php
+++ tests/DeprecatedAutoloaderTest.php
@@ -3,18 +3,17 @@
 /**
  * This file is part of MetaModels/attribute_translatedalias.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeTranslatedAlias
+ * @package    MetaModels/attribute_translatedalias
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_alias/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_translatedalias/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
