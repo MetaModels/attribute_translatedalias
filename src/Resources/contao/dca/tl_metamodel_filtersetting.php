310d7d31df080988518c768b5934824f31433ae8
Ingolf Steinhardt <info@e-spin.de>
2019-12-20 22:29:11 +0100

    add DCA param to use checkbox "all_langs"

---
+++ src/Resources/contao/dca/tl_metamodel_filtersetting.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of MetaModels/attribute_translatedalias.
+ *
+ * (c) 2012-2019 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels/attribute_translatedalias
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_translatedalias/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['simplelookup_palettes']['translatedalias'] =
+    &$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['simplelookup_palettes']['_translated_'];
